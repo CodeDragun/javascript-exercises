f050b318bea0d8c2aba08288f020dfe34f7fbcaa
CodeDragun <141139041+CodeDragun@users.noreply.github.com>
2024-12-02 00:18:03 +0100

    Add solution for exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    
+    if(typeof index === 'string' ){
+        parseInt(index)
+      }
+    if(index == 0){
+        return 0
+    }
+    if(index < 0){
+        return "OOPS"
+    }
 
+  let firstNumber = 1;
+  let prevNumber = 0; 
+  let currentNum;
+  for(let i = 2; i <= index; i++ ){
+    currentNum = firstNumber + prevNumber;
+    prevNumber = firstNumber;
+    firstNumber = currentNum;
+  }
+  return firstNumber
 };
 
 // Do not edit below this line
