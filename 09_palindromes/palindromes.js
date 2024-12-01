19146dac93b8e6b014b1614a052a68d635ec3033
CodeDragun <141139041+CodeDragun@users.noreply.github.com>
2024-12-01 22:43:47 +0100

    Add solution for excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+const alphaNumerics= "abcdefghijklmnopqrstuvwxyz0123456789";
 
+
+const textArr = text.toLowerCase().split('');
+const filteredArr = textArr.filter(word => alphaNumerics.includes(word));
+const newText = filteredArr.join('');
+const reversedText = filteredArr.reverse().join('');
+return reversedText == newText
 };
 
 // Do not edit below this line
