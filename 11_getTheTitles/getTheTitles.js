f4d3975a13df5565bbb03d3f9bda0eb603860e86
CodeDragun <141139041+CodeDragun@users.noreply.github.com>
2024-12-02 01:07:44 +0100

    Add solution for exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    return arr.reduce(((acc, item) => {
+        if(!acc[item.title])
+            acc.push(item.title)
+        return acc
+    }), [])
 
 };
 
