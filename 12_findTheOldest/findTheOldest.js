1b1421dcaa7208ca861f1237b863bd0d63d11661
CodeDragun <141139041+CodeDragun@users.noreply.github.com>
2024-12-06 10:43:58 +0100

    Add solution for excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    
+    return array.reduce((oldest, youngest) =>{
+        let result;
+        const oldestPerson = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const youngestPerson = (youngest.yearOfDeath || currentYear) - youngest.yearOfBirth;
+        
+      return oldestPerson > youngestPerson ? oldest : youngest
+     
+    })
 };
 
 // Do not edit below this line
